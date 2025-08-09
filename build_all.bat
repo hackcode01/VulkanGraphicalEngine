a32fd0da9de6bd75f8f891b5adf273979b5ce254
hackcode01 <andreyrazinwork@yandex.ru>
2025-08-09 22:33:24 +0000

    Added run_editor.bat for run editor on Windows and modified file build_all.bat.

--- build_all.bat
+++ build_all.bat
@@ -1,18 +1,1 @@
-@REM @ECHO OFF
-@REM REM Build everything
-
-@REM ECHO "Building everything..."
-
-@REM PUSHD engine
-@REM CALL build.bat
-@REM POPD
-@REM IF %ERROR_LEVELS% NEQ 0 (echo Error:%ERROR_LEVELS% && exit)
-
-@REM PUSHD editor
-@REM CALL build.bat
-@REM POPD
-@REM IF %ERROR_LEVELS% NEQ 0 (echo Error:%ERROR_LEVELS% && exit)
-
-@REM ECHO "All assemblies build successfully."
-
-cd .\engine\ && .\build.bat && cd ../editor\ && ./build.batbuild.bat
+cd ./engine && ./build.bat && cd ../editor && ./build.bat && cd ../
---
+++ run_editor.bat
@@ -0,0 +1,1 @@
+cd ./bin && ./editor.exe
