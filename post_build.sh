d849f60a1026241c6a93576d6e5f974d946a23ec
SimpleFreak <andreyrazinwork@yandex.ru>
2025-08-26 01:23:13 +0300

    Modified post_build.sh. Add to previoous commit: CMake + post_build.bat or post_build.sh.

--- post_build.sh
+++ post_build.sh
@@ -6,16 +6,16 @@ mkdir -p bin/assets/shaders
 
 echo "Compiling shaders..."
 
-echo "assets/shaders/Builtin.ObjectShader.vert.glsl -> bin/assets/shaders/Builtin.ObjectShader.vert.spv"
-$VULKAN_SDK/bin/glslc -fshader-stage=vert assets/shaders/Builtin.ObjectShader.vert.glsl -o bin/assets/shaders/Builtin.ObjectShader.vert.spv
+echo "assets/shaders/BuiltinObjectShader.vert.glsl -> bin/assets/shaders/BuiltinObjectShader.vert.spv"
+$VULKAN_SDK/bin/glslc -fshader-stage=vert assets/shaders/BuiltinObjectShader.vert.glsl -o bin/assets/shaders/BuiltinObjectShader.vert.spv
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
 then
 echo "Error:"$ERRORLEVEL && exit
 fi
 
-echo "assets/shaders/Builtin.ObjectShader.frag.glsl -> bin/assets/shaders/Builtin.ObjectShader.frag.spv"
-$VULKAN_SDK/bin/glslc -fshader-stage=frag assets/shaders/Builtin.ObjectShader.frag.glsl -o bin/assets/shaders/Builtin.ObjectShader.frag.spv
+echo "assets/shaders/BuiltinObjectShader.frag.glsl -> bin/assets/shaders/BuiltinObjectShader.frag.spv"
+$VULKAN_SDK/bin/glslc -fshader-stage=frag assets/shaders/BuiltinObjectShader.frag.glsl -o bin/assets/shaders/BuiltinObjectShader.frag.spv
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
 then
